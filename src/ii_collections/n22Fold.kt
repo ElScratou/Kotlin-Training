25560bd0c1e821b876e82d7443173fcead1d9a22
ElScratou <sephirot.33@gmail.com>
2019-01-01 18:08:59 +0100

    Fold

--- src/ii_collections/n22Fold.kt
+++ src/ii_collections/n22Fold.kt
@@ -12,10 +12,8 @@ fun whatFoldDoes(): Int {
     return result
 }
 
-fun Shop.getSetOfProductsOrderedByEachCustomer(): Set<Product> {
-    // Return the set of products that were ordered by each of the customers
-    return customers.fold(allOrderedProducts, {
-        orderedByAll, customer ->
-        todoCollectionTask()
-    })
-}
+/**
+ * Return the set of products that were ordered by each of the customers
+ */
+fun Shop.getSetOfProductsOrderedByEachCustomer(): Set<Product> = customers.fold(allOrderedProducts,
+        {orderedByAll, customer -> orderedByAll.intersect(customer.orderedProducts)});
