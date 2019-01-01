9281f8d5e75ffe2df1161f11b1354e0aa301162f
ElScratou <sephirot.33@gmail.com>
2019-01-01 17:54:33 +0100

    Partition

--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -10,7 +10,11 @@ fun example8() {
     negative == listOf(-4, -11)
 }
 
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = customers.filter {
+    val (delivered, undelivered) = it.orders.partition { it.isDelivered }
+    undelivered.size > delivered.size
+}.toSet();
+/*{
     // Return customers who have more undelivered orders than delivered
     todoCollectionTask()
-}
+}*/
