1796307e0e4088ef96d5cb8c01451c0430c53111
ElScratou <sephirot.33@gmail.com>
2019-01-01 17:46:07 +0100

    Max Min

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -5,12 +5,14 @@ fun example4() {
     val longestString = listOf("a", "b").maxBy { it.length }
 }
 
-fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy { it.orders.size } ;
+/*{
     // Return a customer whose order count is the highest among all customers
     todoCollectionTask()
-}
+}*/
 
-fun Customer.getMostExpensiveOrderedProduct(): Product? {
+fun Customer.getMostExpensiveOrderedProduct(): Product? = orders.flatMap { it.products }.maxBy { it.price };
+/*{
     // Return the most expensive product which has been ordered
     todoCollectionTask()
-}
+}*/
